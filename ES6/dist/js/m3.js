2d1f0a892e9e905776c9e8e00670d7d11ddbe0ac
NaerDD <806915230@qq.com>
2023-02-25 16:56:50 +0800

    1

---
+++ ES6/dist/js/m3.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+//默认暴露  默认暴露的属性在default对象里面
+exports.default = {
+  school: 'ATGUIGU',
+  change: function change() {
+    console.log('我们可以改变你!!');
+  }
+};
